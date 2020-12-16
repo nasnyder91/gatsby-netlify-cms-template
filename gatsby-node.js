d35b6c05f1e7538ddcfb2e5610b198fc44ec765c
Nick Snyder <nsnyder@andculture.com>
2020-12-16 14:43:07 -0500

    trial

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,7 +5,7 @@ const { fmImagesToRelative } = require("gatsby-remark-relative-images");
 const fetch = require("node-fetch");
 const fs = require("fs");
 
-exports.sourceNodes = async ({ actions, cache, createNodeId, createContentDigest }) => {
+exports.onPreBootstrap = async () => {
     const response = await fetch(
         "https://sandbox.dev.clover.com/v3/merchants/J9MV77D46ST91/items?access_token=582540d1-2fa6-dd03-7699-e107e6c03c0d&limit=10",
         {
@@ -18,35 +18,129 @@ exports.sourceNodes = async ({ actions, cache, createNodeId, createContentDigest
         .then((response) => response.json())
         .catch((err) => console.log(err));
 
+    // response.elements.forEach(async (item) => {
+    //     await fs.readFile(`./src/pages/inventory/${item.id}.json`, "utf-8", async (err, data) => {
+    //         let itemObject;
+
+    //         if (!err) {
+    //             itemObject = { ...JSON.parse(data), ...item };
+    //         } else {
+    //             itemObject = {
+    //                 ...item,
+    //                 title: item.name,
+    //                 active: false,
+    //                 description: "",
+    //                 image: null,
+    //                 templateKey: "inventory-item-template",
+    //             };
+    //         }
+
+    //         await fs.writeFile(
+    //             `./src/pages/inventory/${item.id}.json`,
+    //             JSON.stringify(itemObject),
+    //             "utf-8",
+    //             (err) => {
+    //                 if (err) {
+    //                     console.log(err);
+    //                 }
+    //             }
+    //         );
+    //     });
+    // });
     response.elements.forEach((item) => {
-        fs.readFile(`./src/pages/inventory/${item.id}.json`, "utf-8", (err, data) => {
-            let itemObject;
+        let fileData;
+        let itemObject;
 
-            if (!err) {
-                itemObject = { ...JSON.parse(data), ...item };
-            } else {
-                itemObject = {
-                    ...item,
-                    title: item.name,
-                    active: false,
-                    description: "",
-                    image: null,
-                    templateKey: "inventory-item-template",
-                };
-            }
+        try {
+            fileData = fs.readFileSync(`./src/pages/inventory/${item.id}.json`, "utf-8");
+        } catch {
+            fileData = {
+                title: item.name,
+                active: false,
+                description: "",
+                image: null,
+                templateKey: "inventory-item-template",
+            };
+        }
 
-            fs.writeFile(
+        itemObject = { ...fileData, ...item };
+
+        try {
+            fs.writeFileSync(
                 `./src/pages/inventory/${item.id}.json`,
                 JSON.stringify(itemObject),
-                "utf-8",
-                (err) => {
-                    if (err) {
-                        throw err;
-                    }
-                }
+                "utf-8"
             );
-        });
+        } catch {
+            console.log(`Could not write file for item with name ${item.name} and id ${item.id}`);
+        }
     });
+};
+
+exports.sourceNodes = async ({ actions, cache, createNodeId, createContentDigest }) => {
+    // const response = await fetch(
+    //     "https://sandbox.dev.clover.com/v3/merchants/J9MV77D46ST91/items?access_token=582540d1-2fa6-dd03-7699-e107e6c03c0d&limit=10",
+    //     {
+    //         method: "GET",
+    //         headers: {
+    //             Accept: "application/json",
+    //         },
+    //     }
+    // )
+    //     .then((response) => response.json())
+    //     .catch((err) => console.log(err));
+    // // response.elements.forEach(async (item) => {
+    // //     await fs.readFile(`./src/pages/inventory/${item.id}.json`, "utf-8", async (err, data) => {
+    // //         let itemObject;
+    // //         if (!err) {
+    // //             itemObject = { ...JSON.parse(data), ...item };
+    // //         } else {
+    // //             itemObject = {
+    // //                 ...item,
+    // //                 title: item.name,
+    // //                 active: false,
+    // //                 description: "",
+    // //                 image: null,
+    // //                 templateKey: "inventory-item-template",
+    // //             };
+    // //         }
+    // //         await fs.writeFile(
+    // //             `./src/pages/inventory/${item.id}.json`,
+    // //             JSON.stringify(itemObject),
+    // //             "utf-8",
+    // //             (err) => {
+    // //                 if (err) {
+    // //                     console.log(err);
+    // //                 }
+    // //             }
+    // //         );
+    // //     });
+    // // });
+    // response.elements.forEach((item) => {
+    //     let fileData;
+    //     let itemObject;
+    //     try {
+    //         fileData = fs.readFileSync(`./src/pages/inventory/${item.id}.json`, "utf-8");
+    //     } catch {
+    //         fileData = {
+    //             title: item.name,
+    //             active: false,
+    //             description: "",
+    //             image: null,
+    //             templateKey: "inventory-item-template",
+    //         };
+    //     }
+    //     itemObject = { ...fileData, ...item };
+    //     try {
+    //         fs.writeFileSync(
+    //             `./src/pages/inventory/${item.id}.json`,
+    //             JSON.stringify(itemObject),
+    //             "utf-8"
+    //         );
+    //     } catch {
+    //         console.log();
+    //     }
+    // });
 };
 
 exports.createPages = ({ actions, graphql }) => {
@@ -70,7 +164,7 @@ exports.createPages = ({ actions, graphql }) => {
         }
     `).then((result) => {
         if (result.errors) {
-            result.errors.forEach((e) => console.error(e.toString()));
+            result.errors.forEach((e) => console.error("ERROR: ", e.toString()));
             return Promise.reject(result.errors);
         }
         console.log(result);
