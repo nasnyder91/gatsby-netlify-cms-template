df3747390f03b5b77f2243638423ba221caf24e0
Nick Snyder <nsnyder@andculture.com>
2021-01-11 19:34:37 -0500

    lets try this

--- functions/pull-and-compare-items.js
+++ functions/pull-and-compare-items.js
@@ -5,6 +5,8 @@ const glob = require("glob");
 // On successful Clover API request, compares and returns items that have been added or changed.
 
 module.exports.handler = async (event, context) => {
+    const devEnv = process.env.NODE_ENV === "development";
+
     const response = await fetch(
         "https://sandbox.dev.clover.com/v3/merchants/J9MV77D46ST91/items?access_token=582540d1-2fa6-dd03-7699-e107e6c03c0d&limit=500",
         {
@@ -28,9 +30,24 @@ module.exports.handler = async (event, context) => {
     const itemsAdded = [];
     const itemsRemoved = [];
 
+    let prodInventoryItemsPath = `${__dirname}public/page-data/items/`;
+    let devInventoryItemsPath = `${__dirname}src/pages/inventory/`;
+
     results.elements.forEach((item) => {
         try {
-            fileData = JSON.parse(fs.readFileSync(__dirname + `src/pages/inventory/${item.id}.json`, "utf-8"));
+            fileData = JSON.parse(
+                fs.readFileSync(
+                    devEnv
+                        ? `${devInventoryItemsPath}${item.id}.json`
+                        : `${prodInventoryItemsPath}${item.id}/page-data.json`,
+                    "utf-8",
+                ),
+            );
+
+            if (!devEnv) {
+                fileData = fileData.result.data.inventoryJson;
+            }
+
             for (const key of Object.keys(item)) {
                 if (item[key] !== fileData[key]) {
                     itemsUpdated.push({ ...item });
@@ -42,7 +59,7 @@ module.exports.handler = async (event, context) => {
         }
     });
 
-    glob.sync(__dirname + "src/pages/inventory/*.json").forEach((file) => {
+    glob.sync(`${inventoryItemsPath}${item.id}.json`).forEach((file) => {
         const itemId = file.split("/").pop().split(".")[0];
 
         if (results.elements.find((item) => item.id === itemId) == null) {
